4865e03867481eddc7993968956f4dd71bbcfe93
Ibrahim Ibrahim <ibrahimahmed.eshag@gmail.com>
2022-10-20 15:46:00 -0700

    Initial Commit

---
+++ Review menu/Review_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Review_menuApp.swift
+//  Review menu
+//
+//  Created by Ibrahim on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Review_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
