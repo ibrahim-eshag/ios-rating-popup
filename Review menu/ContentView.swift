73da488dc38fe4f3d0ef323dd97265d298264e98
Ibrahim Ibrahim <ibrahimahmed.eshag@gmail.com>
2022-10-20 15:58:25 -0700

    initial commit

--- Review menu/ContentView.swift
+++ Review menu/ContentView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    
+    @Environment(\.requestReview) var requestReview
+    @State private var didCoolThingCounter = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(didCoolThingCounter)")
+                .font(.system(size: 200, weight: .heavy, design: .rounded))
+            
+            Button("Do cool thing"){
+                didCoolThingCounter += 1
+                
+                if didCoolThingCounter == 20 {
+                    requestReview()
+                }
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .tint(.pink)
         }
         .padding()
     }
